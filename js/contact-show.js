16dd2179a4d12f114885c41466f441f591465dff
Romain <romain.sanchez@libre-informatique.fr>
2018-02-21 18:03:12 +0100

    [Contact] Improve phone and address display

--- js/contact-show.js
+++ js/contact-show.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.remove-note').click(function() {
     $(this).parents('.contact-note').remove();
   });
+
+  $('.contact-phone, .contact-address').hover(function() {
+    $(this).find('.make-default').toggle();
+  });
 });
