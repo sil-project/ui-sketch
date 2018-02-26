ced32794bc8363e2af898ba175cc7357e48a615a
PapsOu <vivien.frasca@libre-informatique.fr>
2018-02-26 09:06:20 +0100

    [Order] Simplify product select in order creation steps

--- data/Order/create.js
+++ data/Order/create.js
@@ -94,5 +94,58 @@ module.exports = {
                 type: 'text'
             }
         }
+    },
+    list: {
+        options: {
+            actions: []
+        },
+        headers: [
+            {
+                name: 'name',
+                label: 'Nom'
+            }, {
+                name: 'code',
+                label: 'Référence'
+            }, {
+                name: 'active',
+                label: 'Actif'
+            }, {
+                name: 'createdAt',
+                label: 'Créé le'
+            }
+        ],
+        elements: [
+            {
+                name: 'The awesome and incredible product 32',
+                code: 'AWE-INC-0032',
+                active: true,
+                createdAt: '2018-02-20 12:42:00'
+            }, {
+                name: 'The awesome and incredible product 64',
+                code: 'AWE-INC-0064',
+                active: true,
+                createdAt: '2018-02-20 12:42:00'
+            }, {
+                name: 'The awesome and incredible product 128',
+                code: 'AWE-INC-0128',
+                active: true,
+                createdAt: '2018-02-20 12:42:00'
+            }, {
+                name: 'The awesome and incredible product 256',
+                code: 'AWE-INC-0256',
+                active: true,
+                createdAt: '2018-02-20 12:42:00'
+            }, {
+                name: 'The awesome and incredible product 512',
+                code: 'AWE-INC-0512',
+                active: true,
+                createdAt: '2018-02-20 12:42:00'
+            }, {
+                name: 'The awesome and incredible product 1024',
+                code: 'AWE-INC-1024',
+                active: false,
+                createdAt: '2018-02-14 18:36:00'
+            }
+        ]
     }
 };
